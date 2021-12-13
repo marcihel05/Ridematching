23270f55f16b0b943375e7a739ba0d43260c1bbb
marcihel05 <helena.marcius05@gmail.com>
2021-12-13 14:48:49 +0100

    check and fix after crossover

--- solution.py
+++ solution.py
@@ -12,18 +12,20 @@ class Solution:
     
     def initialize(self): #inicijaliziraj početno rješenje - posloži driver.stops i unmatched stops
        ridersCopy = self.riders
-       for i in range(NUM_OF_SOLUTIONS):
-           r = random.randrange(0,len(ridersCopy))
-           rider = ridersCopy[r]
-           ridersCopy.remove(rider)
-           random.shuffle(self.routes)
-           for driver in self.routes:
+       random.shuffle(self.routes)
+       for driver in self.routes:
+           for i in range(len(ridersCopy)):
+               r = random.randrange(len(ridersCopy))
+               rider = ridersCopy[r]
                if driver.taken + rider.numOfPassengers <= driver.capacity: #putnik stane u auto
                    if driver.depTime < rider.depTime and driver.arrivalTime > rider.arrivalTime: #vremenski okvir je okej
                        for stop in driver.stops:
-
-
-
+                           #provjeri dal može
+                           driver.stops.append([rider,rider.start,0]) #na pravi indeks
+                           driver.stops.append([rider,rider.end,1]) #na pravi indeks
+                           ridersCopy.remove(rider)
+       self.unmatched = ridersCopy
+                   
             
         
     def mutate(self):
@@ -50,6 +52,10 @@ class Solution:
         newSolution2.routes = otherSolution.routes
         routeIndex = random.randrange(self.numOfRoutes)
         newSolution1.routes[routeIndex], newSolution2.routes[routeIndex] = newSolution2.routes[routeIndex], newSolution1.routes[routeIndex]
+        if not newSolution1.checkIfFeasibleAfterCross(routeIndex):
+            newSolution1.fix(routeIndex)
+        if not newSolution2.checkIfFeasibleAfterCross(routeIndex):
+            newSolution2.fix(routeIndex)
         return newSolution1, newSolution2
     
     def calculateFitness(self): #racunaj funkciju dobrote (po onoj formuli)
@@ -65,10 +71,19 @@ class Solution:
         val+=time
         return val
 
-    def checkIfFeasible(self): #pogledaj dal više vozača ne vozi istog putnika
-        for route in self.routes:
-            ...
-        return True
+    def checkIfFeasibleAfterCrossAndFix(self, routeIndex): #pogledaj dal više vozača ne vozi istog putnik 0a
+        for i in range(routeIndex+1):
+            for stop1 in self.routes[i].stops:
+                if stop1[2] == 0:
+                    rider = stop2
+                    for j in range(routeIndex+1,len(self.routes)):
+                        for stop2 in self.routes[j].stops:
+                            if stop2 == rider:
+                                stop2.remove(rider)
+                                stop2.remove([rider[0], rider[1],1])
+
+            
+        
     
     def fix(self): #popravi ako nije dopustivo
         ...
