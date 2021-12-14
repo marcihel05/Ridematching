2a670e7839d599be0290449ba382922dddcf3ca0
marcihel05 <helena.marcius05@gmail.com>
2021-12-14 15:39:26 +0100

    fifth mut op done, others started

--- solution.py
+++ solution.py
@@ -10,7 +10,7 @@ class Solution:
         self.unmatched = [] #lista Ridera koji se ne voze
         self.numOfRoutes = 0 #len(self.routes)
     
-    def initialize(self): #inicijaliziraj početno rješenje - posloži driver.stops i unmatched stops
+    def initialize(self): #inicijaliziraj početno rješenje - posloži driver.stops i unmatched stops #DOVRŠITI
        ridersCopy = self.riders
        random.shuffle(self.routes)
        for driver in self.routes:
@@ -25,26 +25,58 @@ class Solution:
                            driver.stops.append([rider,rider.end,1]) #na pravi indeks
                            ridersCopy.remove(rider)
        self.unmatched = ridersCopy
+       self.numOfRoutes = len(self.routes)
                    
             
         
     def mutate(self):
-        ...
+        self.pushBackward()
+        self.pushForward()
+        self.removeInsert()
+        self.transfer()
+        self.swap()
     
     def pushBackward(self): #first mutation operator
-        ...
+        for route in self.routes:
+            r = random.random()
+            if r < MUTATION_RATE:
+                i = random.randrange(len(route.stops))
     
     def pushForward(self): #seconds mutation operator
-        ...
+        for route in self.routes:
+            r = random.random()
+            if r < MUTATION_RATE:
+                i = random.randrange(len(route.stops))
     
     def removeInsert(self): #third mutation operator
-        ...
+        for route in self.routes:
+            r = random.random()
+            if r < MUTATION_RATE:
+                i = random.randrange(len(route.stops))
+                stop = route.stops[i]
+                self.unmatched.append(stop[0])
+                route.stops.remove(stop)
+                route.stops.remove([stop[0], stop[0].end, 1])
+                for rider in self.unmatched:
+                    self.tryToInsert(rider)
+                
     
     def transfer(self): #fourth mutation operator
-        ...
+        for route in self.routes:
+            r = random.random()
+            if r < MUTATION_RATE:
+                i = random.randrange(len(route.stops))
+                if self.tryToInsert(self.routes.index(route), route.stops[i][0]):
+                    route.stops.remove(route.stops[i])
+                    route.stops.remove([route.stops[i][0], route.stops[i][0].end, 1])
     
     def swap(self): #fifth mutation operator
-        ...
+        for route in self.routes:
+            r = random.random()
+            if r < MUTATION_RATE:
+                i = random.randrange(len(route.stops))
+                if route.stops[i][0] != route.stops[i+1][0]:
+                    route.stops[i], route.stops[i+1] = route.stops[i+1], route.stops[i]
 
     def crossover(self, otherSolution):
         newSolution1, newSolution2 = self, otherSolution
@@ -59,7 +91,7 @@ class Solution:
         newSolution2.insertUnmatched()
         return newSolution1, newSolution2
     
-    def calculateFitness(self): #racunaj funkciju dobrote (po onoj formuli)
+    def calculateFitness(self): #racunaj funkciju dobrote (po onoj formuli) #DOVRŠITI
         val = 0
         dist = 0
         time = 0
@@ -73,11 +105,11 @@ class Solution:
         return val
 
     def checkIfFeasibleAfterCrossAndFix(self, routeIndex): #pogledaj dal više vozača ne vozi istog putnika i makni ako ima toga
-        for i in range(routeIndex+1):
+        for i in range(routeIndex):
             for stop1 in self.routes[i].stops:
                 if stop1[2] == 0:
                     rider = stop2
-                    for j in range(routeIndex+1,len(self.routes)):
+                    for j in range(routeIndex,len(self.routes)):
                         for stop2 in self.routes[j].stops:
                             if stop2 == rider:
                                 stop2.remove(rider)
@@ -100,4 +132,16 @@ class Solution:
     
     def insertUnmatched(self):
         for rider in self.unmatched:
-            ...#probaj ga negde ubaciti
+            ...#probaj ga negde ubaciti - slično ka inicijalizacija
+            for driver in self.routes:
+                if driver.taken + rider.numOfPassengers <= driver.capacity: #putnik stane u auto
+                   if driver.depTime < rider.depTime and driver.arrivalTime > rider.arrivalTime: #vremenski okvir je okej
+                       for stop in driver.stops:
+                           #provjeri dal može
+                           ...
+
+    def tryToInsert(self, rider, routeIndex = -1):
+        for route in self.routes:
+            if self.routes.index(route) != routeIndex:
+
+        return False
