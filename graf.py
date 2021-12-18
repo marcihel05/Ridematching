77d200031b29798b85253f9f962f460cb51b425f
enigmacodex <61794379+enigmacodex@users.noreply.github.com>
2021-12-18 19:49:56 +0100

    bolje granice i upute

--- graf.py
+++ graf.py
@@ -1,12 +1,12 @@
 
 import matplotlib.pyplot as plt     #potrebno instalirati
 import pylab
-#import numpy as np
 
+#x - lista listi x koord., y - lista listi y koord., z - lista listi labela
 def main(x,y,z):
     
-    pylab.ylim(-100, 100)
-    pylab.xlim(-100, 100)
+    pylab.xlim(-89, -87)
+    pylab.ylim(41, 43)
     
     pylab.xlabel('X')
     pylab.ylabel('Y')
@@ -21,17 +21,3 @@ def main(x,y,z):
 
     plt.show()
     return
-
-##x = [80,81,90,95]
-##y = [51,45,56,64]
-##z = [1,2,3,4]
-##x1 = [40,60,80,10]
-##y1 = [20,30,40,50]
-##z1 = ['a','b','c','d']
-##x2 = [40,50,40,50]
-##y2 = [10,20,30,50]
-##z2 = [10,20,'e','f']
-##l = [x,x1,x2]
-##k = [y,y1,y2]
-##m = [z,z1,z2]
-##main(l,k,m)
